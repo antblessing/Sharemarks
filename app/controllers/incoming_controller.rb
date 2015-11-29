2fd8b08e393bbce5f43841358870ad38397acb6b
antblessing <antblessing@gmail.com>
2015-11-29 20:25:51 +0000

    mailgun attempt

---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,43 @@
+class IncomingController < ApplicationController
+
+  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+
+
+    # You put the message-splitting and business
+    # magic here. 
+    
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
+    @url = "http://#{params["body-plain"]}"
+    
+    if @user == nil
+      @user = User.create(email: params[:sender], password: "helloworld")
+      @user.save!
+    end
+    
+    if @topic == nil
+      @topic = Topic.create(user: @user, title: params[:subject])
+      @topic.save!
+    end
+    
+    @bookmark = Bookmark.create(url: @url, topic: @topic)
+    @bookmark.save!
+    
+      # Find the user by using params[:sender]
+      # Find the topic by using params[:subject]
+      # Assign the url to a variable after retreiving it from params["body-plain"]
+
+      # Check if user is nil, if so, create and save a new user
+
+      # Check if the topic is nil, if so, create and save a new topic
+
+      # Now that you're sure you have a valid user and topic, build and save a new bookmark
+
+
+    # Assuming all went well. 
+    head 200
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'welcome/about'
   
   root to: 'welcome#index'
+  
+  post :incoming, to: 'incoming#create'
 
 end
