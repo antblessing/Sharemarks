ec34ac229224267898ee84699ec362f884601707
antblessing <antblessing@gmail.com>
2015-12-11 12:44:57 -0800

    front end stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = current_user
+    #@bookmarks = @user.bookmarks
+    @topics = @user.topics
+    #@likes = @user.likes
   end
 end
